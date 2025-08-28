9c1d9bef3e747349439185d72e1715c196517639
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-08-29 04:06:10 +0600

    added hear count dom

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("connected")
+const HeartCount = document.querySelector(".heart-count");
+
+document.querySelectorAll(".heart-icon").forEach(icon => {
+  icon.addEventListener("click", () => {
+    let count = parseInt(HeartCount.innerText, 10) || 0;
+    HeartCount.innerText = count + 1;
+  });
+});
