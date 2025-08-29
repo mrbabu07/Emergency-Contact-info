9c7e20ac78d26ce8f274a8c897f0061838a276d5
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-08-29 09:07:24 +0600

    adding the heart count and copy count and call section work

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
+// heart count 
+
 const HeartCount = document.querySelector(".heart-count");
 
-document.querySelectorAll(".heart-icon").forEach(icon => {
+document.querySelectorAll(".heart-icon").forEach((icon) => {
   icon.addEventListener("click", () => {
-    let count = parseInt(HeartCount.innerText, 10) || 0;
+    let count = parseInt(HeartCount.innerText);
     HeartCount.innerText = count + 1;
   });
 });
+
+// copy count 
+const copyBtns = document.getElementsByClassName("copy-btn");
+
+
+const copyCountSpan = document.querySelector(".copy-parent span");
+
+
+for (let btn of copyBtns) {
+  btn.addEventListener("click", function () {
+    const card = btn.parentElement.parentElement;
+
+    const number = card.getElementsByClassName("card-number")[0].innerText;
+
+    navigator.clipboard.writeText(number).then(() => {
+      alert(`Copied: ${number}`);
+
+      
+      let currentCount = parseInt(copyCountSpan.innerText);
+      copyCountSpan.innerText = currentCount + 1;
+    });
+  });
+}
+
+// call section process 
+
+let coinCount = document.getElementsByClassName("coin-count")[0];
+
+
+let callButtons = document.getElementsByClassName("call-btn");
+
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].addEventListener("click", function () {
+    
+    let btnGroup = this.parentElement;
+
+    
+    let card = btnGroup.parentElement;
+
+   
+    let serviceName = card.getElementsByTagName("h2")[0].innerText;
+
+   
+    let serviceNumber = card.getElementsByClassName("card-number")[0].innerText;
+
+    
+    let coins = parseInt(coinCount.innerText);
+    if (coins < 20) {
+      alert("Not enough coins to make a call!");
+      return; 
+    }
+
+    
+    alert("Calling " + serviceName + " at " + serviceNumber);
+
+    
+    coinCount.innerText = coins - 20;
+  });
+}
+
